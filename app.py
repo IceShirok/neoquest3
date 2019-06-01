469b9a38ab0c2df89c0978214e6488dd870cac45
Susanna <dongsusanna@gmail.com>
2019-06-01 13:30:59 -0400

    Add skills JSON after parsing webpage
    I really liked the skill names and descriptions for Neoquest. I wrote several quick scripts to transform the HTML into a cleaner JSON. Ref: http://www.oocities.org/neoquest_2/skills.html

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+import json
+
 
 def run_app():
-  print('Hello world!')
+    print('Hello world!')
+
+    with open('skills.json') as f:
+        lines = f.readlines()
+        for line in lines:
+            skill = json.loads(line)
+            print(json.dumps(skill, indent=4))
+    f.close()
+
 
 if __name__ == '__main__':
-  run_app()
+    run_app()
