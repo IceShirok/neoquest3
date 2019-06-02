2578c30fe7be2eaa473b2d09f1bfa127fd99331f
Susanna <dongsusanna@gmail.com>
2019-06-02 19:05:27 -0400

    Get rid of the SQLALchemy warning

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 app = Flask(__name__)
