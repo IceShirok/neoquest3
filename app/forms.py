f78eff14427eb972ee08b380f12b6e99ad85261f
Susanna D <IceShirok@users.noreply.github.com>
2019-06-02 13:05:30 -0400

    Add login page and clean up username-specific pages (#1)
    * Add login functionality to webapp
    
    * Clean up the tavern and the party page

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,17 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
 
 class CreatePetForm(FlaskForm):
     pet_list = [
--- app/models.py
+++ app/models.py
@@ -1,12 +1,27 @@
 from sqlalchemy import UniqueConstraint
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 from app import db
+from app import login
 
 
-class User(db.Model):
-    username = db.Column(db.String(64), primary_key=True, index=True, unique=True)
+@login.user_loader
+def load_user(username):
+    return User.query.get(username)
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User={}, email={}>'.format(self.username, self.email)
