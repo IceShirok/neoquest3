1ba3e8c5da1161231945c60e028938c193c89b40
Susanna <dongsusanna@gmail.com>
2019-06-01 20:59:54 -0400

    Fix up data to have some variations

--- db_init.py
+++ db_init.py
@@ -1,6 +1,13 @@
 from app import app, db
 from app.models import User, Pet
 
+from sqlalchemy.sql import text as sa_text
+
+
+db.engine.execute(sa_text('''DELETE FROM user''').execution_options(autocommit=True))
+db.engine.execute(sa_text('''DELETE FROM pet''').execution_options(autocommit=True))
+
+
 users = [
     User(username='katya', email='katya@example.com'),
     User(username='arno', email='arno@example.com'),
@@ -13,7 +20,39 @@ pets = [
         name='Wulgar',
         owner='katya',
         species='Lupe',
-        color='Desert',
+        color='Yellow',
+        gender=1,
+        level=1,
+        max_health=6,
+        current_health=6,
+        strength=6,
+        movement=7,
+        defense=8,
+        intelligence=9,
+        hunger=6,
+        mood=4,
+    ),
+    Pet(
+        name='Devon',
+        owner='katya',
+        species='Techo',
+        color='Shadow',
+        gender=1,
+        level=1,
+        max_health=6,
+        current_health=6,
+        strength=6,
+        movement=7,
+        defense=8,
+        intelligence=9,
+        hunger=6,
+        mood=4,
+    ),
+    Pet(
+        name='Katya',
+        owner='katya',
+        species='Cybunny',
+        color='Blue',
         gender=1,
         level=1,
         max_health=6,
